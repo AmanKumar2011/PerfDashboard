b7b75e8eecb5bd9a80fb1c4eb98c2d0308b73b59
AmanKumar2011 <55837430+AmanKumar2011@users.noreply.github.com>
2022-01-23 12:04:12 +0530

    Array String

--- CoreJava/src/icici/loans/carloans/ArrayStringExercise.java
+++ CoreJava/src/icici/loans/carloans/ArrayStringExercise.java
@@ -9,21 +9,33 @@ public class ArrayStringExercise {
 		int size;
 	//	String str;
 		String[] myArray;
+//		char ch;
+	//	String s;
 		
+		//System.out.println(s);
 		
 		System.out.println("Enter the size of c3 array dynamically");
 		Scanner sc1=new Scanner(System.in);
 		size=sc1.nextInt();
+	//	ch=sc1.next().charAt(0);
+	//	System.out.println(ch);
+	//	System.out.println("Enter the string s");
+	//	s=sc1.next();
+	////	 s=sc1.nextLine();
+	//	System.out.println(s);
+		
+		
 		myArray= new String[size];
-    System.out.println("Enter the String");
+ //   System.out.println("Enter the String");
     
   //  str = sc1.nextLine();
     
   //  System.out.println(str);
     
-    for(int i=0; i<size; i++) 
+    for(int i=0; i<myArray.length; i++) 
     {
         myArray[i] = sc1.next();
+       
         System.out.println(myArray[i]);
    }
     
