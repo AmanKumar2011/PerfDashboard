ee64803c1d95688a5d1808edd7c826f6d003c0e6
AmanKumar2011 <55837430+AmanKumar2011@users.noreply.github.com>
2022-01-28 15:36:51 +0530

    ArrayList

--- CoreJava/src/icici/loans/carloans/ArrayListCollection.java
+++ CoreJava/src/icici/loans/carloans/ArrayListCollection.java
@@ -1,9 +1,11 @@
 package icici.loans.carloans;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Scanner;
 
-public class Collections {
+public class ArrayListCollection {
 	
 	int a, b;	
 	/*
@@ -30,18 +32,23 @@ public class Collections {
 		a1.add(6);
 		a1.add(9);
 		a1.add(10);
+		System.out.println("Index is"+a1.indexOf(2));
+		
 		System.out.println(a1);
 		a1.add(1, 12);
 		System.out.println(a1);
 		a1.set(1, 11);
 		System.out.println(a1);
 		a1.remove(0);
+		a1.remove("6");  //this will work if arraylist is object type
 		System.out.println(a1);
 		
 		System.out.println("Does it contains 9 " + a1.contains(9));
 		System.out.println("What is the index of 6  " +a1.indexOf(6));
 		System.out.println("Is it empty " + a1.isEmpty());
 		System.out.println("What is the size of Arraylist "+a1.size());
+		System.out.println("sublist is " +a1.subList(1,4));
+		
 		
 		ArrayList<Integer> a2= (ArrayList<Integer>) a1.clone();
 		System.out.println("a2 values  "+a2);
@@ -65,7 +72,41 @@ public class Collections {
 			System.out.println(itr.next());
 		}
 		//	a2= a1.clone();
-
+		System.out.println("------------------");
+		a1.clear();
+		System.out.println(a1);
+		
+		System.out.println("------------------");
+		
+	//converting normal array to arraylist
+		
+		 Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the size of the array :: ");
+	      int size = sc.nextInt();
+	      String myArray[] = new String[size];
+	      System.out.println("Enter elements of the array (Strings) :: ");
+	      for(int i=0; i<size; i++) {
+	         myArray[i] = sc.next();
+	      }
+	 //     System.out.println(myArray);
+	      System.out.println(Arrays.toString(myArray));
+	      ArrayList<String> myList = new ArrayList<String>(Arrays.asList(myArray));
+	      System.out.println(myList);
+	      for(int i=0;i<myList.size();i++)
+			{
+				System.out.println(myList.get(i));
+				
+			}
+	      
+	    //converting arraylist to normal array
+	      
+	      System.out.println("Enter the element that is to be added:");
+	      String element;
+	      element= sc.next();
+	      
+	      myList.add(element);
+	      myArray = myList.toArray(myArray);
+	      System.out.println(Arrays.toString(myArray));
 	}
 
 }
